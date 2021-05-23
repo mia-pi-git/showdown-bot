857751d59018f99b827ce1eda10a4248188e0b1d
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-05-23 12:45:06 -0500

    Namespace some types

--- src/global-types.ts
+++ src/global-types.ts
@@ -5,3 +5,8 @@
 type PSRoom = import('./room').PSRoom;
 type PSUser = import('./user').PSUser;
 type CommandBase = import('./commands').CommandBase;
+
+namespace PS {
+    export type PLine = import('./main').PLine;
+    export type PLineHandler = import('./main').PLineHandler;
+}
